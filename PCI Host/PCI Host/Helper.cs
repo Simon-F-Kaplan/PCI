9dba21f3e23cce5cf87ae1e02aedf7a08ef118b6
Simon-F-Kaplan <simon.ferris@kaplan.co.uk>
2015-11-02 10:13:30 +0000

    Initial commit

---
+++ PCI Host/PCI Host/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PCI_Host
+{
+    public class Helper
+    {
+        public enum Response
+        {
+            Success = 0,
+            Failure = -1
+        }
+    }
+}
